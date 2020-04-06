340690c585e39cab2ea9ad6f755b81044be5b5da
Alex <aclifford336@gmail.com>
2020-04-06 12:57:31 +0100

    added form and todo_item partials - then edited the show.html.erb page - can add todos now

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,7 +2,7 @@ class TodoItemsController < ApplicationController
   before_action :set_todo_list
 
   def create
-    @todo_item = @todo_list.todo_item.create(todo_item_params)
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
   end
 
